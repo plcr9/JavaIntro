23f314452d4f52a0d7e3cb88f00bdf05ed8743db
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 14:39:11 +0100

    Update tree.java

--- tree.java
+++ tree.java
@@ -1,1 +1,13 @@
-
+public class Tree {
+  public static void main(String[] args) {
+    // introduce self and print tree
+    System.out.println("Hello!");
+    System.out.println("Let's plant a tree!");
+    System.out.println("   ####   ");
+    System.out.println("  ######  ");
+    System.out.println(" ######## ");
+    System.out.println("  ######  ");
+    System.out.println("    ||    ");
+    System.out.println("    ||    ");
+  }
+}
