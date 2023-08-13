9cd1a502e64e031b2533451c0c4bfe5b19c48a95
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 14:56:59 +0100

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,1 +1,6 @@
-
+public class Song {
+  public static void main(String[] args) {
+    String openingLyrics = "Today is gonna be the day that they're gonna throw it back to you";
+    System.out.println(openingLyrics);
+  }
+}
