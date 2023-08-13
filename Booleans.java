4c5b975c655b44f55eed5d3d18416561fbca2426
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 14:52:25 +0100

    Update Booleans.java

--- Booleans.java
+++ Booleans.java
@@ -1,6 +1,6 @@
 public class Booleans {
   public static void main(String[] args) {
-    boolean intsCanHoldDecimals = false;
+    boolean intsCanHoldDecimals = true;
     System.out.println(intsCanHoldDecimals);
   }
 }
