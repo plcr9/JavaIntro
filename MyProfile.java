d0115d3657d2e3d21768ace4be9d78ec9abd0e4f
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 15:12:45 +0100

    Update MyProfile.java

--- MyProfile.java
+++ MyProfile.java
@@ -1,1 +1,15 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Laura";
+        int age = 25;
+        double desiredSalary = 80000.00;
+        char gender = 'f';
+        boolean lookingForJob = true;
 
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(desiredSalary);
+        System.out.println(gender);
+        System.out.println(lookingForJob);
+    }
+}
