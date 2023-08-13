5cc269f06d1d8b35d3b93ad7dd82be16309f3c4b
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 14:27:15 +0100

    Update java_facts.java

--- java_facts.java
+++ java_facts.java
@@ -1,1 +1,8 @@
-
+public class JavaFacts {
+  public static void main(String[] args) {
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes hava a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+    System.out.println("Programming is...fun!");
+  }
+}
