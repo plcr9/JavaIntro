5072281f6b472978edadee0ba930191c6642c181
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-13 14:59:14 +0100

    Update Mess.java

--- Mess.java
+++ Mess.java
@@ -1,1 +1,9 @@
-
+public class Java {
+  public static void main(String[] args) {
+    int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
+  }
+}
