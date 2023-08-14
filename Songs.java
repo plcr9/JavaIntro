5b3e742e1ff741b9bae94c6c2351ba68c2d9848a
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-14 11:22:48 +0100

    Update Songs.java

--- Songs.java
+++ Songs.java
@@ -1,1 +1,11 @@
+public class Song {
+  public static void main(String[] args) {
+    String line1 = "Nah Nah Nah Nah Nah Nah Nah Nah Nah yeah";
+    String line2 = "Nah Nah Nah Nah Nah Nah Nah Nah Nah hey Jude";
+    String line3 = "Nah Nah Nah Nah Nah Nah Nah Nah Nah hey Jude";
 
+    System.out.println(line1.equals(line2));
+    System.out.println(line2.equals(line3));
+
+  }
+}
