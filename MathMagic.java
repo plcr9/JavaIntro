8a6153fde469e603e3a69f308ff84f032b1f0d90
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-14 11:38:06 +0100

    Update MathMagic.java

--- MathMagic.java
+++ MathMagic.java
@@ -1,1 +1,14 @@
+public class MathMagic {
+  public static void main(String[] args) {
 
+    int myNumber = 15;
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
+
+  }
+}
