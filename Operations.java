a5df7d8155ae8547110da109c57ea5b412adb90e
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-14 10:29:33 +0100

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,1 +1,7 @@
-
+public class Operations {
+  public static void main(String[] args) {
+    int expression1 = 5 % 2 - (4 * 2 - 1);
+    int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+    int expression3 = 5 * 4 % 3 - 2 + 1;
+  }
+}
