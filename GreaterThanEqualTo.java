bda330206185455fe616fae617f83bd9f24fc11e
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-14 10:46:12 +0100

    Update GreaterThanEqualTo.java

--- GreaterThanEqualTo.java
+++ GreaterThanEqualTo.java
@@ -1,1 +1,13 @@
+public class GreaterThanEqualTo {
+  public static void main(String[] args) {
+    double recommendedWaterIntake = 8;
+    double daysInChallenge = 30;
+    double yourWaterIntake = 235.5;
 
+    double totalRecommendedAmount = recommendedWaterInTake * daysInChallenge;
+
+    boolean isChallengeComplete = yourWaterIntake >= totalRecommendedAmount;
+
+    System.out.println(isChallengeComplete);
+  }
+}
