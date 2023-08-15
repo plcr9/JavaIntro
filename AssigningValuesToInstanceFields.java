80a24b9808232ba877fb946e344c0314fffb1899
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 13:01:23 +0100

    Update AssigningValuesToInstanceFields.java

--- AssigningValuesToInstanceFields.java
+++ AssigningValuesToInstanceFields.java
@@ -1,1 +1,12 @@
+public class AssigningValuesToInstanceFields {
+  String productType;
 
+  public Store(String product) {
+    productType = product;
+  }
+
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade");
+    System.out.println(lemonadeStand.productType);
+  }
+}
