7d46a1cb72ec9faf7b8381ac5a32c63e099d32f8
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 12:54:22 +0100

    Update Constructors.java

--- Constructors.java
+++ Constructors.java
@@ -1,1 +1,13 @@
+public class Constructors {
+  public Store() {
+    System.out.println("I am inside the constructor method.");
+  }
 
+  public static void main(String[] args) {
+    System.out.println("Start of the main method.");
+
+    Store lemonadeStand = new Store();
+
+    System.out.println(lemonadeStand);
+  }
+}
