6102523d5d1aad49429a4f578766b26a3d8089cd
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 13:15:21 +0100

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,1 +1,12 @@
+public class Dog {
+  String breed;
+  boolean hasOwner;
+  int age;
 
+  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+}
