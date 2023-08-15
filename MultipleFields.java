f3ceb7bd98edad8b208712cc45670cd3f852aeb5
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 13:08:37 +0100

    Update MultipleFields.java

--- MultipleFields.java
+++ MultipleFields.java
@@ -3,13 +3,13 @@ public class MultipleFields {
   int inventoryCount;
   double inventoryPrice;
 
-  public Store(String product, int count, double price) {
+  public MultipleFields(String product, int count, double price) {
     productType = product;
     inventoryCount = count;
     inventoryPrice = price;
   }
 
   public static void main(String[] args) {
-    Store cookieShop = new Store("cookies", 12, 3.75);
+    MultipleFields cookieShop = new MultipleFields("cookies", 12, 3.75);
   }
 }
