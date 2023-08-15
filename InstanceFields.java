a18d9d4cf6caf14114904e495c0fdd3aa81c29a3
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 12:57:02 +0100

    Update InstanceFields.java

--- InstanceFields.java
+++ InstanceFields.java
@@ -1,1 +1,15 @@
+public class InstanceFields {
+  String productType;
 
+  public Store() {
+    System.out.println("I am inside the constructor method.");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("This code is inside the main method.");
+
+    Store lemonadeStand = new Store();
+
+    System.out.println(lemonadeStand);
+  }
+}
