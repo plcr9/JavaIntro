9505346a4eb3c802ba13a6def93a6ba86394c0ff
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 12:58:53 +0100

    Update ConstructorParameters.java

--- ConstructorParameters.java
+++ ConstructorParameters.java
@@ -1,1 +1,11 @@
+public class ConstructorParameters {
+  String productType;
 
+  public Store(String product) {
+    productType = product;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
