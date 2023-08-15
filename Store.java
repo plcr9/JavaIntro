804f5dbf95914088be94735ab397cfd59b3de0fd
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 12:51:34 +0100

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,1 +1,20 @@
+public class Store {
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
 
+  public store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade", 42, .99);
+    Store cookieShop = new Store("cookies", 12, 3.75);
+
+    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+
+    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+  }
+}
